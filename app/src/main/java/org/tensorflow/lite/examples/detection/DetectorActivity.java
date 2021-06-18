8788cd3a07d5b0c770b1fc201f2dae9862bae0dc
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2021-06-19 04:50:15 +0700

    ONG: translate the label

---
+++ app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
@@ -0,0 +1,394 @@
+/*
+ * Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tensorflow.lite.examples.detection;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.media.ImageReader.OnImageAvailableListener;
+import android.os.SystemClock;
+import android.speech.tts.TextToSpeech;
+import android.util.Size;
+import android.util.TypedValue;
+import android.widget.Toast;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import org.tensorflow.lite.examples.detection.customview.OverlayView;
+import org.tensorflow.lite.examples.detection.customview.OverlayView.DrawCallback;
+import org.tensorflow.lite.examples.detection.env.BorderedText;
+import org.tensorflow.lite.examples.detection.env.ImageUtils;
+import org.tensorflow.lite.examples.detection.env.Logger;
+import org.tensorflow.lite.examples.detection.tflite.Detector;
+import org.tensorflow.lite.examples.detection.tflite.TFLiteObjectDetectionAPIModel;
+import org.tensorflow.lite.examples.detection.tracking.MultiBoxTracker;
+
+/**
+ * An activity that uses a TensorFlowMultiBoxDetector and ObjectTracker to detect and then track
+ * objects.
+ */
+public class DetectorActivity extends CameraActivity implements OnImageAvailableListener {
+  private static final Logger LOGGER = new Logger();
+
+  // Configuration values for the prepackaged SSD model.
+  private static final int TF_OD_API_INPUT_SIZE = 300;
+  private static final boolean TF_OD_API_IS_QUANTIZED = true;
+  private static final String TF_OD_API_MODEL_FILE = "detect.tflite";
+  private static final String TF_OD_API_LABELS_FILE = "labelmap.txt";
+  private static final DetectorMode MODE = DetectorMode.TF_OD_API;
+  // Minimum detection confidence to track a detection.
+  private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.5f;
+  private static final boolean MAINTAIN_ASPECT = false;
+  private static final Size DESIRED_PREVIEW_SIZE = new Size(640, 480);
+  private static final boolean SAVE_PREVIEW_BITMAP = false;
+  private static final float TEXT_SIZE_DIP = 10;
+  OverlayView trackingOverlay;
+  private Integer sensorOrientation;
+
+  private Detector detector;
+
+  private long lastProcessingTimeMs;
+  private Bitmap rgbFrameBitmap = null;
+  private Bitmap croppedBitmap = null;
+  private Bitmap cropCopyBitmap = null;
+
+  private boolean computingDetection = false;
+
+  private long timestamp = 0;
+
+  private Matrix frameToCropTransform;
+  private Matrix cropToFrameTransform;
+
+  private MultiBoxTracker tracker;
+
+  private BorderedText borderedText;
+
+  /* Custom variable */
+  TextToSpeech tts;
+  List<Detector.Recognition> tempMappedRecognitions =
+          new ArrayList<Detector.Recognition>();
+
+//  private String translate(String text){
+//    switch (text){
+//      case "person" : return "orang";
+//      case "bicycle" : return "sepeda";
+//      case "car" : return "mobil";
+//      case "motorcycle" : return "sepeda motor";
+//      case "airplane": return "pesawat terbang";
+//      case "bus": return "bis";
+//      case "train": return "kereta";
+//      case "truck": return "truk";
+//      case "boat": return "perahu";
+//      case "traffic light": return "lampu lalu lintas";
+//      case "fire hydrant": return "alat pemadam kebakaran";
+//      case "stop sign": return "tanda berhenti";
+//      case "parking meter": return "meteran parkir";
+//      case "bench": return "bangku";
+//      case "bird": return "burung";
+//      case "cat": return "kucing";
+//      case "dog": return "anjing";
+//      case "horse": return "kuda";
+//      case "sheep": return "domba";
+//      case "cow": return "lembu";
+//      case "elephant": return "gajah";
+//      case "bear": return "beruang";
+//      case "zebra": return "zebra";
+//      case "giraffe": return "jerapah";
+//      case "backpack": return "ransel";
+//      case "umbrella": return "payung";
+//      case "handbag": return "tas tangan";
+//      case "tie": return "dasi";
+//      case "suitcase": return "koper";
+//      case "frisbee": return "piring lempar";
+//      case "skis": return "ski";
+//      case "snowboard": return "papan seluncur";
+//      case "sports ball": return "bola sport";
+//      case "kite": return "layang-layang";
+//      case "baseball bat": return "tongkat pemukul baseball";
+//      case "baseball glove": return "sarung baseball";
+//      case "skateboard": return "papan luncur";
+//      case "surfboard": return "papan selancar";
+//      case "tennis racket": return "raket tenis";
+//      case "bottle": return "botol";
+//      case "wine glass": return "gelas anggur";
+//      case "cup": return "cangkir";
+//      case "fork": return "garpu";
+//      case "knife": return "pisau";
+//      case "spoon": return "sendok";
+//      case "bowl": return "banana";
+//      case "apple": return "apel";
+//      case "sandwich": return "sandwich";
+//      case "orange": return "jeruk";
+//      case "broccoli": return "brokoli";
+//      case "carrot": return "wortel";
+//      case "hot dog": return "Hot Dog";
+//      case "pizza": return "Pizza";
+//      case "donut": return "donat";
+//      case "cake": return "kue";
+//      case "chair": return "kursi";
+//      case "couch": return "sofa";
+//      case "potted plant": return "tanaman di dalam pot";
+//      case "bed": return "tempat tidur";
+//      case "dining table": return "meja makan";
+//      case "toilet": return "toilet";
+//      case "tv": return "televisi";
+//      case "laptop": return "laptop";
+//      case "mouse": return "mouse";
+//      case "remote": return "remote";
+//      case "keyboard": return "papan ketik";
+//      case "cell phone": return "telpon selular";
+//      case "microwave": return "microwave";
+//      case "oven": return "oven";
+//      case "toaster": return "pemanggang roti";
+//      case "sink": return "wastafel";
+//      case "refrigerator": return "kulkas";
+//      case "book": return "buku";
+//      case ""
+//
+//      default: return "";
+////      pemanggang roti
+////      wastafel
+////              kulkas
+////???
+////      buku
+////              jam
+////      vas
+////              gunting
+////      boneka beruang
+////      pengering rambut
+////      sikat gigi
+//    }
+//  }
+
+  @Override
+  public void onPreviewSizeChosen(final Size size, final int rotation) {
+    final float textSizePx =
+        TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, TEXT_SIZE_DIP, getResources().getDisplayMetrics());
+    borderedText = new BorderedText(textSizePx);
+    borderedText.setTypeface(Typeface.MONOSPACE);
+
+    tracker = new MultiBoxTracker(this);
+
+    int cropSize = TF_OD_API_INPUT_SIZE;
+
+    try {
+      detector =
+          TFLiteObjectDetectionAPIModel.create(
+              this,
+              TF_OD_API_MODEL_FILE,
+              TF_OD_API_LABELS_FILE,
+              TF_OD_API_INPUT_SIZE,
+              TF_OD_API_IS_QUANTIZED);
+      cropSize = TF_OD_API_INPUT_SIZE;
+    } catch (final IOException e) {
+      e.printStackTrace();
+      LOGGER.e(e, "Exception initializing Detector!");
+      Toast toast =
+          Toast.makeText(
+              getApplicationContext(), "Detector could not be initialized", Toast.LENGTH_SHORT);
+      toast.show();
+      finish();
+    }
+
+    previewWidth = size.getWidth();
+    previewHeight = size.getHeight();
+
+    sensorOrientation = rotation - getScreenOrientation();
+    LOGGER.i("Camera orientation relative to screen canvas: %d", sensorOrientation);
+
+    LOGGER.i("Initializing at size %dx%d", previewWidth, previewHeight);
+    rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Config.ARGB_8888);
+    croppedBitmap = Bitmap.createBitmap(cropSize, cropSize, Config.ARGB_8888);
+
+    frameToCropTransform =
+        ImageUtils.getTransformationMatrix(
+            previewWidth, previewHeight,
+            cropSize, cropSize,
+            sensorOrientation, MAINTAIN_ASPECT);
+
+    cropToFrameTransform = new Matrix();
+    frameToCropTransform.invert(cropToFrameTransform);
+
+    trackingOverlay = (OverlayView) findViewById(R.id.tracking_overlay);
+    trackingOverlay.addCallback(
+        new DrawCallback() {
+          @Override
+          public void drawCallback(final Canvas canvas) {
+            tracker.draw(canvas);
+            if (isDebug()) {
+              tracker.drawDebug(canvas);
+            }
+          }
+        });
+
+    tracker.setFrameConfiguration(previewWidth, previewHeight, sensorOrientation);
+  }
+
+  @Override
+  protected void processImage() {
+    ++timestamp;
+    final long currTimestamp = timestamp;
+    trackingOverlay.postInvalidate();
+
+    // No mutex needed as this method is not reentrant.
+    if (computingDetection) {
+      readyForNextImage();
+      return;
+    }
+    computingDetection = true;
+    LOGGER.i("Preparing image " + currTimestamp + " for detection in bg thread.");
+
+    rgbFrameBitmap.setPixels(getRgbBytes(), 0, previewWidth, 0, 0, previewWidth, previewHeight);
+
+    readyForNextImage();
+
+    final Canvas canvas = new Canvas(croppedBitmap);
+    canvas.drawBitmap(rgbFrameBitmap, frameToCropTransform, null);
+    // For examining the actual TF input.
+    if (SAVE_PREVIEW_BITMAP) {
+      ImageUtils.saveBitmap(croppedBitmap);
+    }
+
+    runInBackground(
+        new Runnable() {
+          @Override
+          public void run() {
+            LOGGER.i("Running detection on image " + currTimestamp);
+            final long startTime = SystemClock.uptimeMillis();
+            final List<Detector.Recognition> results = detector.recognizeImage(croppedBitmap);
+            lastProcessingTimeMs = SystemClock.uptimeMillis() - startTime;
+
+            cropCopyBitmap = Bitmap.createBitmap(croppedBitmap);
+            final Canvas canvas = new Canvas(cropCopyBitmap);
+            final Paint paint = new Paint();
+            paint.setColor(Color.RED);
+            paint.setStyle(Style.STROKE);
+            paint.setStrokeWidth(2.0f);
+
+            float minimumConfidence = MINIMUM_CONFIDENCE_TF_OD_API;
+            switch (MODE) {
+              case TF_OD_API:
+                minimumConfidence = MINIMUM_CONFIDENCE_TF_OD_API;
+                break;
+            }
+
+            final List<Detector.Recognition> mappedRecognitions =
+                new ArrayList<Detector.Recognition>();
+
+            for (final Detector.Recognition result : results) {
+              final RectF location = result.getLocation();
+              if (location != null && result.getConfidence() >= minimumConfidence) {
+                canvas.drawRect(location, paint);
+                tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                  @Override
+                  public void onInit(int status) {
+                    if (status != TextToSpeech.ERROR) {
+                      tts.setLanguage(new Locale("id", "ID"));
+                      tts.speak(result.getTitle(), TextToSpeech.QUEUE_FLUSH, null);
+                      Toast.makeText(getApplicationContext(), result.getTitle(), Toast.LENGTH_SHORT).show();
+                    }
+                  }
+                });
+//                for (final Detector.Recognition tempResult : tempMappedRecognitions) {
+//                  if(result.getTitle().equals(tempResult.getTitle())){
+//
+//                  }
+//                  tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+//                    @Override
+//                    public void onInit(int status) {
+//                      if (status != TextToSpeech.ERROR) {
+//                        tts.setLanguage(new Locale("id", "ID"));
+//                        tts.speak(result.getTitle(), TextToSpeech.QUEUE_FLUSH, null);
+//                        Toast.makeText(getApplicationContext(), result.getTitle(), Toast.LENGTH_SHORT).show();
+//                      }
+//                    }
+//                  });
+//                }
+                cropToFrameTransform.mapRect(location);
+
+                result.setLocation(location);
+                mappedRecognitions.add(result);
+              }
+            }
+
+            tracker.trackResults(mappedRecognitions, currTimestamp);
+            System.out.println("OUTPUT = " + mappedRecognitions + "><" + tempMappedRecognitions);
+            System.out.println("IS EQUAL? = " + mappedRecognitions.equals(tempMappedRecognitions));
+
+            trackingOverlay.postInvalidate();
+
+            computingDetection = false;
+
+            runOnUiThread(
+                new Runnable() {
+                  @Override
+                  public void run() {
+                    showFrameInfo(previewWidth + "x" + previewHeight);
+                    showCropInfo(cropCopyBitmap.getWidth() + "x" + cropCopyBitmap.getHeight());
+                    showInference(lastProcessingTimeMs + "ms");
+                  }
+                });
+          }
+        });
+  }
+
+  @Override
+  protected int getLayoutId() {
+    return R.layout.tfe_od_camera_connection_fragment_tracking;
+  }
+
+  @Override
+  protected Size getDesiredPreviewFrameSize() {
+    return DESIRED_PREVIEW_SIZE;
+  }
+
+  // Which detection model to use: by default uses Tensorflow Object Detection API frozen
+  // checkpoints.
+  private enum DetectorMode {
+    TF_OD_API;
+  }
+
+  @Override
+  protected void setUseNNAPI(final boolean isChecked) {
+    runInBackground(
+        () -> {
+          try {
+            detector.setUseNNAPI(isChecked);
+          } catch (UnsupportedOperationException e) {
+            LOGGER.e(e, "Failed to set \"Use NNAPI\".");
+            runOnUiThread(
+                () -> {
+                  Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+                });
+          }
+        });
+  }
+
+  @Override
+  protected void setNumThreads(final int numThreads) {
+    runInBackground(() -> detector.setNumThreads(numThreads));
+  }
+}
